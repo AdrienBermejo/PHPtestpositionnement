6c938bedb9ec1a8b65898e84795557837feccb15
Adrien Bermejo Garcia <adrienbermejogarcia@leschassagnes.net>
2024-10-25 18:59:56 +0200

    Dernier puis week end

--- main.php
+++ main.php
@@ -7,11 +7,7 @@ function calculerSousTotal($produit){
     return $sous_total;
 }
 
-function main(){
-    $banane = ["nom" => "Banane", "prix" => 2, "quantite" => 8];
-    $poire = ["nom" => "Poire", "prix" => 12, "quantite" => 5];
-    $pomme = ["nom" => "Pomme", "prix" => 1, "quantite" => 10];
-
+function main($banane,$poire,$pomme){
     $prixBanane = calculerSousTotal($banane);
     $prixPoire = calculerSousTotal($poire);
     $prixPomme = calculerSousTotal($pomme);
@@ -19,14 +15,16 @@ function main(){
     if ($somme>50){
         $reduction = ($somme*(10/100));
         $total = $somme - $reduction;
-        echo "Le total de réduction sur votre panier est de ".$reduction."€. ";
+        echo "Le total de réduction sur votre panier est de ".$reduction."€. </br>";
         echo "Le total de votre commande est de ".$total."€. ";
     }  
     else{
         echo "le total de votre commande est de ".$somme."€. ";
     }
-    
+    return 0;
 }
 
-
-?>
+$banane = ["nom" => "Banane", "prix" => 2, "quantite" => 8];
+$poire = ["nom" => "Poire", "prix" => 12, "quantite" => 5];
+$pomme = ["nom" => "Pomme", "prix" => 1, "quantite" => 10];
+main($banane,$poire,$pomme);
